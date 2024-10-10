1dca98da3cac5d8d36172bd449fa6f13c9a4e886
Brent Gibson <bgibson1618@gmail.com>
2024-10-10 04:25:10 -0500

    added type-check assertions

--- PyStructs/Arrays/MergeSorted.py
+++ PyStructs/Arrays/MergeSorted.py
@@ -1,4 +1,9 @@
-def merge_sorted_easy(arr1, arr2):
+def merge_sorted_easy(arr1: list[int], arr2: list[int]) -> list[int]:
+    assert isinstance(arr1, list) and isinstance(arr2, list), 'Inputs must be lists'
+    assert (all(isinstance(item, int)
+                and not isinstance(item, bool) for item in arr1)
+            and all(isinstance(item, int)
+                    and not isinstance(item, bool) for item in arr2)), 'Inputs must contain only integers'
     temp_array = arr1 + arr2
     temp_array.sort()
     return temp_array
@@ -20,8 +25,8 @@ def merge_sorted(arr1, arr2):
     return temp_array + arr1[i:] + arr2[j:]
 
 
-#r1, r2 = [0, 3, 4, 31], [4, 6, 30]
-#print(merge_sorted_easy(r1, r2))
+r1, r2 = [0, 3, 4, 31], [1, 2, 3, 4, 5]
+print(merge_sorted_easy(r1, r2))
 
-r1, r2 = [1, 2, 3, 7], 0
-print(merge_sorted(r1, r2))
+# r1, r2 = [1, 2, 3, 7], 0
+# print(merge_sorted(r1, r2))
